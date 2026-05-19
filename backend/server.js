dde27bd797c78f0d5466a98713b81db383ed92dc
Javi <pingubdn@gmail.com>
2026-05-19 22:46:36 +0200

    Resize video before sending to fal PixVerse

--- backend/server.js
+++ backend/server.js
@@ -254,12 +254,14 @@ app.post(
 
       const submitResult = await fal.queue.submit(modelId, {
         input: {
-        video_url: targetUpload.secure_url,
+        video_url: targetUpload.secure_url.replace(
+  '/upload/',
+  '/upload/w_720,h_1280,c_limit,f_mp4/'
+),
         image_url: faceUpload.secure_url,
         swap_mode: 'person',
         keyframe_id: 1,
         original_sound_switch: true,
-        resolution: '540p',
 },
       });
 
